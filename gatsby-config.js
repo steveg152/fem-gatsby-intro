d4c33c1619ea351f734eaf0f9d0ea18eb70c1201
Stephen Greco <steveox152@gmail.com>
2020-08-20 05:59:07 -0400

    Webpack Bundle Analyzer

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,5 +37,14 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
   ],
 };
